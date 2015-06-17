557115f5d5688361752355c7beee985421cf26da
ctranxuan <ctranxuan@gmail.com>
2015-06-17 10:41:08 +0200

    fix(headers): added the Apache2 license header

--- src/Exporter.js
+++ src/Exporter.js
@@ -1,3 +1,20 @@
+/**
+ *
+ *    Copyright 2015 streamdata.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 var Exporter = (function() {
   'use strict';
 
@@ -7,7 +24,7 @@ var Exporter = (function() {
      * Google Closure Compiler helpers (used only to make the minified file smaller)
      * @param {string} publicPath
      * @param {*} object
-     */ 
+     */
     exportSymbol: function(owner, publicPath, object) {
       var tokens = publicPath.split(".");
       var target = owner;
@@ -16,7 +33,7 @@ var Exporter = (function() {
       }
       this.exportProperty(target, tokens[tokens.length - 1], object);
     },
-    
+
     /**
      * @function
      * Google Closure Compiler helpers (used only to make the minified file smaller)
@@ -27,6 +44,6 @@ var Exporter = (function() {
     exportProperty: function(owner, publicName, object) {
       owner[publicName] = object;
     }
-    
+
   };
 })();
